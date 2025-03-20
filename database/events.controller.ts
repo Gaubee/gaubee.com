4eae86fde79e8eae65ada6eabd7c235e15e895d3
Gaubee <GaubeeBangeel@Gmail.com>
2025-03-20 09:58:49 +0800

    ♻️ event现在也带上前缀

--- database/events.controller.ts
+++ database/events.controller.ts
@@ -15,12 +15,21 @@ export const getAllEvents = func_remember(async () => {
         const info = matter(entry.readText());
         const createdAt = new Date(info.data.date || entry.stats.birthtimeMs);
         return {
+          fileEntry: entry,
+          originMetadata: info.data,
           metadata: { ...info.data, createdAt },
+          markdownContent: info.content,
           htmlContent: await markdownToHtml(info.content, {
             filepath: entry.path,
           }),
         };
       })
     )
-  ).filter((it) => it !== undefined);
+  )
+    .filter((it) => it !== undefined)
+    .sort(
+      (a, b) =>
+        b.metadata.createdAt.getTime() - a.metadata.createdAt.getTime() ||
+        b.fileEntry.name.localeCompare(a.fileEntry.name)
+    );
 });
--- events/web-design-principles.md
+++
@@ -1,6 +0,0 @@
----
-layout: layouts/event.njk
-date: 2023-03-27T01:45:54.933Z
----
-
-[Web Design Principles](https://w3ctag.github.io/design-principles/) 这篇文章提供了 Web 平台的接口设计最佳实践
