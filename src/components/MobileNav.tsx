a2c83efeba008821f4f49f1de0beecb1293b6665
Gaubee <GaubeeBangeel@Gmail.com>
2025-09-22 14:52:41 +0800

    ♻️ 优化MobileNav的使用体验，现在能直接返回上一个页面

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -15,7 +15,6 @@ export default function MobileNav({ title, children }: Props) {
       <MobileTopbar
         title={title}
         onMenuClick={() => {
-          console.log("qqq");
           setIsOpen(true);
         }}
       />
--- src/components/MobileTopbar.tsx
+++ src/components/MobileTopbar.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { ArrowLeft, Menu } from 'lucide-react';
-import { cn } from '@/lib/utils';
+import { cn } from "@/lib/utils";
+import { ArrowLeft, Menu } from "lucide-react";
+import React, { useEffect, useState } from "react";
 
 interface Props {
   title?: string;
@@ -10,12 +10,50 @@ interface Props {
 }
 
 const MobileTopbar: React.FC<Props> = ({
-  title = '',
+  title = "",
   className,
   onMenuClick,
-  showBackButton = true,
+  showBackButton,
 }) => {
+  const nav = typeof window === "object" ? window.navigation : undefined;
+  const [showBack, setShowBack] = useState(showBackButton ?? false);
+  useEffect(() => {
+    if (showBackButton == null && typeof nav == "object") {
+      const binding = () => {
+        setShowBack(nav.canGoBack);
+      };
+      nav.addEventListener("currententrychange", binding);
+      binding();
+      return () => {
+        nav.removeEventListener("currententrychange", binding);
+      };
+    }
+  }, [showBackButton]);
   const handleBackClick = () => {
+    try {
+      if (typeof nav == "object") {
+        const reversedEntries = nav
+          .entries()
+          .filter(
+            (historyEntry) =>
+              historyEntry.index < (nav.currentEntry?.index ?? 0),
+          )
+          .reverse();
+        const currentUrl = new URL(nav.currentEntry?.url ?? location.href);
+        nav.currentEntry?.index;
+        for (const entry of reversedEntries) {
+          if (!entry.url) {
+            continue;
+          }
+          const historyUrl = new URL(entry.url);
+          if (historyUrl.pathname !== currentUrl.pathname) {
+            nav.traverseTo(entry.key, {});
+            return;
+          }
+        }
+      }
+    } catch {}
+
     window.history.back();
   };
 
@@ -24,10 +62,10 @@ const MobileTopbar: React.FC<Props> = ({
       id="mobile-topbar"
       className={cn(
         "sticky top-0 z-50 flex h-14 items-center justify-between border-b border-zinc-200 bg-white/80 px-4 backdrop-blur-sm dark:border-zinc-800 dark:bg-zinc-900/80 lg:hidden",
-        className
+        className,
       )}
     >
-      {showBackButton ? (
+      {showBack ? (
         <button type="button" onClick={handleBackClick} aria-label="Go back">
           <ArrowLeft size={24} />
         </button>
